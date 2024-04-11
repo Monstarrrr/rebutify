238b7e7eb2a275bbd0273c8b6ce980fcb54d0f9f
Siegfried Porterfield <siegfriedporterfield@gmail.com>
2024-04-12 00:23:03 +0200

    get admin panel path from .env

--- rebutify/urls.py
+++ rebutify/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+import os
+
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path(os.getenv("DJANGO_ADMIN_PATH", "admin/"), admin.site.urls),
 ]
