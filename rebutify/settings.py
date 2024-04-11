5cd3b0d8e362229fea49b04efa74123bd7a7da1e
Siegfried Porterfield <siegfriedporterfield@gmail.com>
2024-04-11 22:55:54 +0200

    fix insecure default key passing

--- rebutify/settings.py
+++ rebutify/settings.py
@@ -10,8 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 from typing import List
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-20hoto9#6bdhv(n&*@4%m_-l3a9vn9gpwr74$-#at-+(&-#+wr"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
