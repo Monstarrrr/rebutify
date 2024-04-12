73425581d5698548633f2eb453861f019f9c6f53
Monstar <hugo.bonpain@gmail.com>
2024-04-12 21:51:40 +0200

    Vite config; Cleanup

--- rebutify-fe/src/App.tsx
+++ rebutify-fe/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <div>
--- rebutify-fe/vite.config.ts
+++ rebutify-fe/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "192.168.1.22",
+    host: "0.0.0.0",
   }
 })
