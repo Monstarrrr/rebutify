78176f69f5541e57126aaa16432d7d26dc812128
Siegfried Porterfield <siegfriedporterfield@gmail.com>
2024-04-13 00:15:57 +0200

    added alivecheck

--- core/urls.py
+++ core/urls.py
@@ -1,1 +1,10 @@
-urlpatterns: list[None] = []
+from django.http import HttpResponse
+from django.urls import path
+
+urlpatterns = [
+    path(
+        "alivecheck/",
+        lambda request: HttpResponse("", content_type="text/plain"),
+        name="alivecheck",
+    ),
+]
