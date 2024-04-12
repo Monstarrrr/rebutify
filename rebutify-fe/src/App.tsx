78f02b60c23dcaeb9d0e38ecf790a2dcba3b8e54
Monstar <hugo.bonpain@gmail.com>
2024-04-12 20:17:44 +0200

    Init front-end

---
+++ rebutify-fe/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Rebutify</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
