d232108745223d04c717cf83d267a67cd6d26f77
Siegfried Porterfield <siegfriedporterfield@gmail.com>
2024-04-17 21:46:55 +0200

    restructured urls files for uniform drf usage

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,1 +1,30 @@
-# Create your views here.
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets
+from rest_framework.response import Response
+
+from .serializers import GroupSerializer, UserSerializer
+
+
+class StatusViewSet(viewsets.ViewSet):
+    def list(self, request):
+        return Response(status=200)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+
+    queryset = User.objects.all().order_by("-date_joined")
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+
+    queryset = Group.objects.all().order_by("name")
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
--- backend/rebutify/settings.py
+++ backend/rebutify/settings.py
@@ -61,6 +61,13 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ]
+}
+
 ROOT_URLCONF = "rebutify.urls"
 
 TEMPLATES = [
