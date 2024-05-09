c9337e8c43aed4d818a62f649606ff648a5642a1
Siegfried Porterfield <siegfriedporterfield@gmail.com>
2024-05-09 09:07:34 +0200

    improve regristration form field names

--- backend/core/forms.py
+++ backend/core/forms.py
@@ -8,4 +8,4 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ["username", "email", "password", "confirmPassword"]
