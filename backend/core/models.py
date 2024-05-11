5a3944a5ab81d265988c13972c88cc5676b8fced
Siegfried Porterfield <siegfriedporterfield@gmail.com>
2024-05-11 17:40:12 +0200

    fix: make correct fields nullable

--- backend/core/models.py
+++ backend/core/models.py
@@ -10,33 +10,35 @@ OWNER_MAX_LEN = 255
 class Tags(models.Model):
     tagName: models.CharField = models.CharField(max_length=TAG_NAME_MAX_LEN)
     count: models.IntegerField = models.IntegerField()
-    excerptPostId: models.IntegerField = models.IntegerField()
-    wikiPostId: models.IntegerField = models.IntegerField()
+    excerptPostId: models.IntegerField = models.IntegerField(null=True)
+    wikiPostId: models.IntegerField = models.IntegerField(null=True)
     isModeratorOnly: models.BooleanField = models.BooleanField()
     isRequired: models.BooleanField = models.BooleanField()
 
 
 class Posts(models.Model):
     postTypeId: models.IntegerField = models.IntegerField()
-    acceptedAnswerId: models.IntegerField = models.IntegerField()
-    parentId: models.IntegerField = models.IntegerField()
+    acceptedAnswerId: models.IntegerField = models.IntegerField(null=True)
+    parentId: models.IntegerField = models.IntegerField(null=True)
     creationDate: models.DateField = models.DateField()
-    deletionDate: models.DateField = models.DateField()
+    deletionDate: models.DateField = models.DateField(null=True)
     score: models.IntegerField = models.IntegerField()
-    viewcount: models.IntegerField = models.IntegerField()
+    viewcount: models.IntegerField = models.IntegerField(null=True)
     body: models.TextField = models.TextField()  # render as HTML
-    ownerUserId: models.IntegerField = models.IntegerField()
-    ownerDisplayName: models.CharField = models.CharField(max_length=OWNER_MAX_LEN)
-    lastEditorUserId: models.IntegerField = models.IntegerField()
-    lastEditorDisplayName: models.CharField = models.CharField(
-        max_length=LAST_EDITOR_MAX_LEN
+    ownerUserId: models.IntegerField = models.IntegerField(null=True)
+    ownerDisplayName: models.CharField = models.CharField(
+        max_length=OWNER_MAX_LEN, null=True
     )
-    lastEditDate: models.DateField = models.DateField()
+    lastEditorUserId: models.IntegerField = models.IntegerField(null=True)
+    lastEditorDisplayName: models.CharField = models.CharField(
+        max_length=LAST_EDITOR_MAX_LEN, null=True
+    )
+    lastEditDate: models.DateField = models.DateField(null=True)
     lastActivityDate: models.DateField = models.DateField()
     title: models.CharField = models.CharField(max_length=TITLE_MAX_LEN)
     tags: models.ManyToManyField = models.ManyToManyField(Tags, related_name="posts")
-    answerCount: models.IntegerField = models.IntegerField()
-    commentCount: models.IntegerField = models.IntegerField()
-    favoriteCount: models.IntegerField = models.IntegerField()
-    closedDate: models.DateField = models.DateField()
-    communityOwnedDate: models.DateField = models.DateField()
+    answerCount: models.IntegerField = models.IntegerField(null=True)
+    commentCount: models.IntegerField = models.IntegerField(null=True)
+    favoriteCount: models.IntegerField = models.IntegerField(null=True)
+    closedDate: models.DateField = models.DateField(null=True)
+    communityOwnedDate: models.DateField = models.DateField(null=True)
