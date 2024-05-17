efd2fa5a3a4305bc656b58b57cca246d2b742ef1
Monstar <hugo.bonpain@gmail.com>
2024-05-18 01:30:12 +0200

    [#1] CORS Allowed origins (pt 2)

--- backend/rebutify/settings.py
+++ backend/rebutify/settings.py
@@ -48,12 +48,15 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "core",
     "rest_framework",
     "rebutify",
 ]
 
 MIDDLEWARE = [
+    # CorsMiddleware should be placed as high as possible
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -61,6 +64,13 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://rebutify.org",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://0.0.0.0:3000",
 ]
 
 REST_FRAMEWORK = {
