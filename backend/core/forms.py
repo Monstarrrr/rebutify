8fc75942844235224739c9afad3ad111e53db1c6
Siegfried Porterfield <siegfriedporterfield@gmail.com>
2024-05-21 20:41:14 +0200

    fix: readded fields expected by UserCreationForm

--- backend/core/forms.py
+++ backend/core/forms.py
@@ -8,4 +8,4 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ["username", "email"]
+        fields = ["username", "email", "password1", "password2"]
