b0cac346ca079f09b8499b8ed99ed4a182f4b26b
purple-void <152819753+purple-void@users.noreply.github.com>
2024-05-30 15:10:09 -0400

    Added debug check for activation link

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,12 +1,15 @@
+from django.conf import settings
 from django.urls import path
 
 from .views import ActivateUser, success
 
-urlpatterns = [
-    path("", success, name="index"),
-    path(
-        "activate/<uid>/<token>",
-        ActivateUser.as_view({"get": "activation"}),
-        name="activation",
-    ),
-]
+urlpatterns = [path("", success, name="index")]
+
+if settings.DEBUG:
+    urlpatterns.append(
+        path(
+            "activate/<uid>/<token>",
+            ActivateUser.as_view({"get": "activation"}),
+            name="activation",
+        )
+    )
--- frontend/app/(routes)/(auth)/layout.tsx
+++ frontend/app/(routes)/(auth)/layout.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
 
-export default function AuthLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       This is the auth layout :D ~ I am in every Auth pages
