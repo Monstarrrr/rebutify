20cafd81c1010b9145af017f7995afa434e6418e
Monstar <31903668+Monstarrrr@users.noreply.github.com>
2024-07-02 23:04:06 +0200

    deleting account; showing self posts<3

--- frontend/app/(routes)/profile/page.tsx
+++ frontend/app/(routes)/profile/page.tsx
@@ -1,13 +1,17 @@
 'use client'
+import { Post } from '@/types'
 import { deleteSelfAccount } from '@/api/auth'
 import { useAppSelector } from '@/store/hooks'
 import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
+import Link from 'next/link'
+import { getPosts } from '@/api/posts'
 
 export default function Profile() {
   const user = useAppSelector((state) => state.user)
   const [password, setPassword] = useState('')
   const router = useRouter()
+  const [posts, setPosts] = useState<Post[] | null>(null)
 
   useEffect(() => {
     if (!user.id) {
@@ -23,13 +27,36 @@ export default function Profile() {
       password && (await deleteSelfAccount(password))
       router.push('/')
     } catch (error: any) {
-      console.error('no')
+      console.error(
+        error.response?.data?.detail ??
+          error.response?.data ??
+          error.response ??
+          error,
+      )
     }
   }
+  useEffect(() => {
+    let fetchApi = async () => {
+      try {
+        let response = await getPosts()
+        response = response.filter((post: Post) => post.ownerUserId === user.id)
+        console.log(`# response :`, response)
+        setPosts(response)
+      } catch (error: any) {
+        console.error('# Error fetching posts: ', error.response.data)
+      }
+    }
+    fetchApi()
+  }, [user.id])
 
   return (
     <div>
       <h1>Profile</h1>
+      <hr />
+      <br />
+      <h2>My info</h2>
+      <hr style={{ border: 'none', borderTop: '1px dotted black' }} />
+      <br />
       <table>
         <tbody>
           <tr>
@@ -46,6 +73,23 @@ export default function Profile() {
           </tr>
         </tbody>
       </table>
+      <br />
+      <h2>My posts</h2>
+      <hr style={{ border: 'none', borderTop: '1px dotted black' }} />
+      <br />
+      <ul>
+        {posts &&
+          posts.map((post) => (
+            <Link href={`/argument/${post.id}`} key={post.id}>
+              {post.title}
+              <br />
+            </Link>
+          ))}
+      </ul>
+      <br />
+      <h2>Settings</h2>
+      <hr style={{ border: 'none', borderTop: '1px dotted black' }} />
+      <br />
       <input onChange={handlePassword} type='password' placeholder='Password' />
       <button style={{ background: 'red' }} onClick={handleDelete}>
         Delete account
--- frontend/app/_api/posts/getPosts.ts
+++ frontend/app/_api/posts/getPosts.ts
@@ -1,8 +1,12 @@
 import api from '@/api/api'
 
-export const getPosts = async () => {
+export const getPosts = async (
+  self: Boolean | undefined = false,
+  id: number | undefined,
+) => {
   try {
-    const response = await api.get('api/posts')
+    const endpoint = self ? 'api/posts/' : 'api/posts'
+    const response = await api.get(endpoint)
     return response.data
   } catch (error) {
     console.error('# "Get posts" request failed: ', error)
