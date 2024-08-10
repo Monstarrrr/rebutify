ba795ad786a6b3ca88f8bf8b0c0ba0a22489458c
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-08-11 01:16:45 +0530

    add fn to get jwt

--- backend/core/tests/test_vote.py
+++ backend/core/tests/test_vote.py
@@ -1,8 +1,18 @@
 from django.contrib.auth.models import User
 from django.test import Client, TestCase
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class VoteTests(TestCase):
+    # Reference: https://django-rest-framework-simplejwt.readthedocs.io/en/latest/creating_tokens_manually.html
+    def get_tokens_for_user(self, user):
+        refresh = RefreshToken.for_user(user)
+
+        return {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }
+
     def setUp(self):
         self.client = Client()
         self.username1 = "testuser1"
