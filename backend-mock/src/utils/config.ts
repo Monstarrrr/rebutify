c603d2dfeaa58c081d4f92b8b0922b15be127b2a
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-08-12 01:19:22 +0530

    cleanup config

--- backend-mock/src/utils/config.ts
+++ backend-mock/src/utils/config.ts
@@ -2,8 +2,6 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 export const PORT = process.env.MOCK_API_PORT || 3000
-export const BASE_URL = `http://localhost:${PORT}`
-export const JWT_SECRET = process.env.MOCK_JWT_SECRET || 'klas73oh534dd3a'
-export const JWT_EXPIRES_IN = process.env.MOCK_JWT_EXPIRES_IN || '1d'
-export const JWT_REFRESH_EXPIRES_IN =
-  process.env.MOCK_JWT_REFRESH_EXPIRES_IN || '1d'
+export const JWT_SECRET = process.env.MOCK_JWT_SECRET
+export const JWT_EXPIRES_IN = process.env.MOCK_JWT_EXPIRES_IN
+export const JWT_REFRESH_EXPIRES_IN = process.env.MOCK_JWT_REFRESH_EXPIRES_IN
