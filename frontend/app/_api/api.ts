9781e6e6f8a8472c604da828b189797a172f6101
Monstar <31903668+Monstarrrr@users.noreply.github.com>
2024-08-11 18:00:55 +0200

    Uncommented code that was temporarily commented for testing

--- frontend/app/_api/api.ts
+++ frontend/app/_api/api.ts
@@ -17,41 +17,37 @@ api.interceptors.request.use(
     if (req.url && req.url[req.url.length - 1] !== '/') {
       req.url += '/'
     }
-    // if (req.requiresAuth) {
-    // temp
-    const accessToken = localStorage.getItem('access_token')
-    const refreshToken = localStorage.getItem('refresh_token')
-    if (
-      accessToken &&
-      accessToken !== 'null' &&
-      accessToken !== 'undefined' &&
-      !isTokenExpired(accessToken)
-    ) {
-      console.log(`# accessToken is not expired.`)
-      // Add the access token to the request headers
-      req.headers['authorization'] = `Bearer ${accessToken}`
-      return req
+    if (req.requiresAuth) {
+      const accessToken = localStorage.getItem('access_token')
+      const refreshToken = localStorage.getItem('refresh_token')
+      if (
+        accessToken &&
+        accessToken !== 'null' &&
+        accessToken !== 'undefined' &&
+        !isTokenExpired(accessToken)
+      ) {
+        console.log(`# accessToken is not expired.`)
+        // Add the access token to the request headers
+        req.headers['authorization'] = `Bearer ${accessToken}`
+        return req
+      }
+      console.log(`# accessToken is expired.`)
+      localStorage.removeItem(`access_token`)
+      if (
+        !refreshToken ||
+        refreshToken === 'null' ||
+        refreshToken === 'undefined'
+      ) {
+        console.log(`# No valid refresh token found.`)
+        return req
+      }
+      // Check for in-flight refresh requests and start one if required
+      refreshTokenPromise ??= refreshTheToken(refreshToken)
+      const newAccessToken = await refreshTokenPromise
+      refreshTokenPromise = null // clean in-flight state
+      localStorage.setItem('access_token', newAccessToken)
+      req.headers['authorization'] = `Bearer ${newAccessToken}`
     }
-    console.log(`# accessToken is expired.`)
-    localStorage.removeItem(`access_token`)
-    if (
-      !refreshToken ||
-      refreshToken === 'null' ||
-      refreshToken === 'undefined'
-    ) {
-      console.log(`# No valid refresh token found.`)
-      return req
-    }
-    // Check for in-flight refresh requests and start one if required
-    refreshTokenPromise ??= refreshTheToken(refreshToken)
-    const newAccessToken = await refreshTokenPromise
-    refreshTokenPromise = null // clean in-flight state
-    localStorage.setItem('access_token', newAccessToken)
-    // req.headers['authorization'] = newAccessToken
-    //   ? `Bearer ${newAccessToken}`
-    //   : ''
-    req.headers['authorization'] = `Bearer ${newAccessToken}`
-    // }
 
     console.log('# Intercepted request:', req)
     return req
