ad0d874dfc53ce62951aced8eb0d7a6d68dea02e
Monstar <31903668+Monstarrrr@users.noreply.github.com>
2024-08-12 22:08:54 +0200

    Cleanup logs

--- frontend/app/_api/auth/register.ts
+++ frontend/app/_api/auth/register.ts
@@ -1,13 +1,11 @@
 import api from '@/api/api'
 
 export const register = async (data: any) => {
-  try {
-    console.log('Register request started')
-    const response = await api.post('/auth/users/', { ...data })
-    console.log('Register request done')
-    return response
-  } catch (error: any) {
-    console.error('# Register request failed: ', error.response.data ?? error)
-    throw error
-  }
+	try {
+		const response = await api.post('/auth/users/', { ...data })
+		return response
+	} catch (error: any) {
+		console.error('# Register request failed: ', error.response.data ?? error)
+		throw error
+	}
 }
