a4d6dc94bf1f3941b0c2f8fc7b096e9064cb413a
Monstar <31903668+Monstarrrr@users.noreply.github.com>
2024-08-13 19:42:52 +0200

    Enabled cors

--- backend-mock/src/app.ts
+++ backend-mock/src/app.ts
@@ -1,12 +1,22 @@
 import * as express from 'express'
 import routes from './routes'
 import { errorHandler, notFoundRoute, requestLogger } from './utils/middleware'
+import cors = require('cors')
+import { corsHeaders } from './utils/config'
 
 const mockApi = express()
 
 // Middleware
 mockApi.use(express.json())
 mockApi.use(requestLogger)
+// Enable CORS based on the corsOptions configuration
+mockApi.use('*', (_, res, next) => {
+	corsHeaders.forEach((option) => {
+		const key = Object.keys(option)[0]
+		res.setHeader(key, option[key])
+	})
+	next()
+})
 
 // Routes
 mockApi.use('/api/posts', routes.postsRouter)
--- backend-mock/src/utils/config.ts
+++ backend-mock/src/utils/config.ts
@@ -5,3 +5,8 @@ export const PORT = process.env.MOCK_API_PORT || 3000
 export const JWT_SECRET = process.env.MOCK_JWT_SECRET
 export const JWT_EXPIRES_IN = process.env.MOCK_JWT_EXPIRES_IN
 export const JWT_REFRESH_EXPIRES_IN = process.env.MOCK_JWT_REFRESH_EXPIRES_IN
+export const corsHeaders = [
+	{ 'Access-Control-Allow-Origin': process.env.CLIENT_URL },
+	{ 'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS' },
+	{ 'Access-Control-Allow-Headers': 'Content-Type, Authorization' },
+]
