3426c3db2ee82f41bbddea77eb5555be5bf52dd9
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-08-16 03:48:17 +0530

    add openapi spec for getPosts

--- backend-mock/src/controllers/posts.ts
+++ backend-mock/src/controllers/posts.ts
@@ -1,5 +1,18 @@
 import * as express from 'express'
 
+/**
+ * @openapi
+ * /api/posts:
+ *   get:
+ *     summary: Get all posts
+ *     description: Get the list of posts in the System.
+ *     tags: [Posts]
+ *     responses:
+ *       200:
+ *         description: Retrieved posts successfully.
+ *       500:
+ *         description: Internal server error.
+ */
 export const getPosts = (_req: express.Request, res: express.Response) => {
   return res.json({ route: 'GET /api/posts' })
 }
