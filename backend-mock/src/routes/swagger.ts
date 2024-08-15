45f82c006d9941e1f93346c6fd943379ad3e77fc
Monstar <31903668+Monstarrrr@users.noreply.github.com>
2024-08-16 00:14:46 +0200

    Added swagger dark mode

--- backend-mock/src/routes/swagger.ts
+++ backend-mock/src/routes/swagger.ts
@@ -3,6 +3,7 @@ import swaggerJsdoc = require('swagger-jsdoc')
 import swaggerUi = require('swagger-ui-express')
 import path = require('path')
 import { swaggerOptions } from '@/utils/config'
+import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes'
 
 // Define the Swagger router
 const swaggerRouter = express.Router()
@@ -11,6 +12,13 @@ const swaggerRouter = express.Router()
 // swagger-jsdoc generates an OpenAPI specs from JSDoc comments
 const swaggerSpec = swaggerJsdoc(swaggerOptions)
 
+// Define the Swagger theme
+const theme = new SwaggerTheme()
+const options = {
+  explorer: true,
+  customCss: theme.getBuffer(SwaggerThemeNameEnum.DARK),
+}
+
 // Serve the Swagger UI with the generated OpenAPI specs
-swaggerRouter.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+swaggerRouter.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec, options))
 export default swaggerRouter
