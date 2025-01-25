044f883792774d9916d4edfc83cd6200d37f61e0
Monstar <31903668+Monstarrrr@users.noreply.github.com>
2025-01-25 14:52:20 +0100

    revalidatePath attempt

--- backend/rebutify/settings.py
+++ backend/rebutify/settings.py
@@ -202,6 +202,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
