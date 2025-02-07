988dd50ffb980cc2d143667914e4a4dc11587dcd
Monstar <31903668+Monstarrrr@users.noreply.github.com>
2025-02-07 02:20:19 +0100

    error handling for backend

--- backend/core/typesense/utils/get_client.py
+++ backend/core/typesense/utils/get_client.py
@@ -4,20 +4,24 @@ import typesense
 
 
 def get_client():
-    return typesense.Client(
-        {
-            "nodes": [
-                {
-                    "host": os.getenv(
-                        "TYPESENSE_HOST", ""
-                    ),  # Docker container will expose this to localhost
-                    "port": os.getenv("TYPESENSE_PORT", ""),  # Typesense default port
-                    "protocol": os.getenv("TYPESENSE_PROTOCOL", ""),
-                }
-            ],
-            "api_key": os.getenv("TYPESENSE_SECRET_KEY", ""),
-            "connection_timeout_seconds": 10,
-            "num_retries": 3,
-            "retry_interval_seconds": 1,
-        }
-    )
+    try:
+        return typesense.Client(
+            {
+                "nodes": [
+                    {
+                        "host": os.getenv(
+                            "TYPESENSE_HOST", ""
+                        ),  # Docker container will expose this to localhost
+                        "port": os.getenv("TYPESENSE_PORT", ""),  # Typesense default port
+                        "protocol": os.getenv("TYPESENSE_PROTOCOL", ""),
+                    }
+                ],
+                "api_key": os.getenv("TYPESENSE_SECRET_KEY", ""),
+                "connection_timeout_seconds": 10,
+                "num_retries": 3,
+                "retry_interval_seconds": 1,
+            }
+        )
+    except Exception as e:
+        print(f"❌ Error connecting to Typesense: {e}")
+        raise
