dca9925c75dacff58f79d9fd32c04d1259a8daf8
Monstar <31903668+Monstarrrr@users.noreply.github.com>
2025-02-07 02:50:47 +0100

    Increase timeout

--- backend/core/typesense/utils/get_client.py
+++ backend/core/typesense/utils/get_client.py
@@ -1,11 +1,19 @@
 import os
-
+import requests
 import typesense
 
 
 def get_client():
+    host = os.getenv("TYPESENSE_HOST", "")
+    port = os.getenv("TYPESENSE_PORT", "")
+    protocol = os.getenv("TYPESENSE_PROTOCOL", "")
+    api_key = os.getenv("TYPESENSE_SECRET_KEY", "")
+    
+    if not host or not port or not protocol or not api_key:
+        raise Exception(f"Missing config: Host: {host}, Port: {port}, Protocol: {protocol}, API Key: {api_key}")
+    
     try:
-        return typesense.Client(
+        client = typesense.Client(
             {
                 "nodes": [
                     {
@@ -17,11 +25,13 @@ def get_client():
                     }
                 ],
                 "api_key": os.getenv("TYPESENSE_SECRET_KEY", ""),
-                "connection_timeout_seconds": 10,
+                "connection_timeout_seconds": 60,
                 "num_retries": 3,
                 "retry_interval_seconds": 1,
             }
         )
+        
+        return client
     except Exception as e:
         print(f"❌ Error connecting to Typesense: {e}")
         raise
