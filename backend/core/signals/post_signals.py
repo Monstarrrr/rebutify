f6d0ff604dc496955ffddbb5a6c360974cd19aeb
Monstar <31903668+Monstarrrr@users.noreply.github.com>
2025-02-14 21:46:33 +0100

    Removed unused import

--- backend/core/signals/post_signals.py
+++ backend/core/signals/post_signals.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.contrib.auth.models import User
 from django.core.mail import send_mail
 from django.db.models.signals import post_save
 from django.dispatch import receiver
